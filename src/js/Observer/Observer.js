6f7474568d14fb46d257d3bab1087003c876ac22
Olga-Viktorova <epam.volha.viktarava@gmail.com>
2016-12-12 03:19:44 +0300

    Design pattern Observer

---
+++ src/js/Observer/Observer.js
@@ -0,0 +1,5 @@
+export default function Observer(){
+  this.update = function(){
+    // ...
+  };
+}
--- src/js/home.js
+++ src/js/home.js
@@ -1,7 +1,23 @@
 import style from '../style/page.less';
+import Subject from './Observer/Subject' 
 //import test from '../testjson/test.json';
+
+function extend( obj, extension ){
+  for ( var key in extension ){
+    obj[key] = extension[key];
+  }
+}
+
 let button = document.getElementById('shownewsbutton');
+
+extend(button, new Subject());
+
 button.onclick = function(){
+	  button.notify( 'test');
+}
+button.onclick = addNewObserver;
+
+function addNewObserver(){
 	button.setAttribute("style", "display:none");
 	require.ensure(['./shownews'], function(require){
 	 	let shownews = require('./shownews');
@@ -9,5 +25,14 @@ button.onclick = function(){
 	})
 };
 
-let shownews = require('../testjson/test.json');
+
+//button.onclick = function(){
+//	button.setAttribute("style", "display:none");
+//	require.ensure(['./shownews'], function(require){
+//	 	let shownews = require('./shownews');
+//	 	shownews();
+//	})
+//};
+
+//let shownews = require('../testjson/test.json');
 
